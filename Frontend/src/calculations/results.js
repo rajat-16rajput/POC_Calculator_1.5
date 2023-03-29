f2695827ef0dc047dd97e774821a38b59f37c05d
rajat-16rajput <67589444+rajat-16rajput@users.noreply.github.com>
2023-03-29 16:58:28 +0530

    Add files via upload

--- Frontend/src/calculations/results.js
+++ Frontend/src/calculations/results.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Route } from "react-router-dom";
 
 import "./myStyles.css";
 import axios from "axios";
@@ -24,24 +24,30 @@ function Results() {
       console.error(err);
     });
 
+
   return (
     <>
       <div>
-        <h1>Results!</h1>
+        <h1>History!</h1>
       </div>
 
       <ul>
         {/* USE OF LISTS AND KEYS IN TO DISPLAY THE ARRAY ELEMENTS */}
-        {/* {records.map((record, index) => (
-          <li key={index}>{record}</li>
-        ))} */}
+        {/* PASSING THE DATA USING QUERY PARAMETER */}
+        {records.map((record, index) => (
+          <li key={index}>
+            <a href={`http://localhost:3000/?last_calculation=` + record}>
+              {record}
+            </a>
+          </li>
+        ))}
 
-        <li>
-          {/* PASSING THE DATA USING QUERY PARAMETER */}
+        {/* <li>
           <a href="/?last_calculation= 77/77">77/7</a>
-          <br></br>
-          <a href="/?last_calculation=55*89">55x89</a>
         </li>
+        <li>
+          <a href="/?last_calculation=55*89">55x89</a>
+        </li> */}
       </ul>
 
       <div>
@@ -53,3 +59,4 @@ function Results() {
 }
 
 export default Results;
+
